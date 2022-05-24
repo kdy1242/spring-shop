f49588608499670fcb29e80c78631e9e587625a9
kdy1242 <dy12311229@gmail.com>
2022-05-24 10:48:40 +0900

    OR 조건 처리하기

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -59,4 +59,14 @@ class ItemRepositoryTest {
             System.out.println(item.toString());
         }
     }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세설명5");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
 }
