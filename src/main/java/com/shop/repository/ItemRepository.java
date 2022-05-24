6762a6149189d5a7032ba97d4fc1467f62619b57
kdy1242 <dy12311229@gmail.com>
2022-05-24 11:03:49 +0900

    @Query 이용한 검색 처리

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,4 +15,8 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByPriceLessThan(Integer price);
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+    // @Param 어노테이션을 이용하여 파라미터로 넘어온 값을 JPQL 에 들어갈 변수로 지정할 수 있음 
 }
